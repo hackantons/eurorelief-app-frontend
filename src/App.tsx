1f99e493a59e969ad2b2d1b41ca94bf6e41be521
nico_martin <mail@nicomartin.ch>
2020-07-24 20:46:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+import { Provider, useStoreState, useActions } from 'unistore-hooks';
+import { H as Router } from 'react-router-dom';
+
+import './App.css';
+
+import { actions, store } from '@app/store';
+import { State } from '@app/store/types';
+
+import IntlLink from '@app/intl/IntlLink';
+import { Logo } from '@app/theme';
+import Menu from '@comp/App/Menu';
+import Content from '@comp/Page/Content';
+import Map from '@comp/Map/Map';
+import Settings from '@comp/App/Settings/Settings';
+
+const App = () => {
+  const { intlLocale, intlMessages }: State = useStoreState([
+    'intlLocale',
+    'intlMessages',
+  ]);
+
+  const { setIdbLoved } = useActions(actions);
+
+  useEffect(() => {
+    setIdbLoved();
+  }, []);
+
+  return (
+    <IntlProvider locale={intlLocale} messages={intlMessages}>
+      <div className="app">
+        <IntlLink href="/" className="app__controls app__controls--logo">
+          <Logo className="app__logo" />
+        </IntlLink>
+        <Menu className="app__controls app__controls--menu" />
+        <Settings
+          className="app__controls app__controls--settings"
+          settingsClassName="app__settings"
+        />
+        <Content className="app__content" />
+        <Map className="app__map" />
+      </div>
+    </IntlProvider>
+  );
+};
+
+ReactDOM.render(
+  <Provider value={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.querySelector('#app')
+);
