b6fca7c93cad023ee563e12b566daa384f3675d4
nico_martin <mail@nicomartin.ch>
2020-09-03 10:01:30 +0200

    changed lugout url

--- src/app/vendor/api.ts
+++ src/app/vendor/api.ts
@@ -9,7 +9,7 @@ export const getUser = () => axios.get(`${API_BASE}user/`);
 
 export const postUser = data => axios.post(`${API_BASE}user/`, data);
 
-export const postUserLogout = () => axios.post(`${API_BASE}user/logout/`);
+export const postAuthLogout = () => axios.post(`${API_BASE}auth/logout/`);
 
 export const postAuthSignIn = (uuid: string, password: string) =>
   axios.post(`${API_BASE}auth/signin/`, {
